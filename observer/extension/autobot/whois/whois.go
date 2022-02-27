e004e8869cb169b4398845ef048c85accd1fff58
Aleksandr Golodyuk <akzarfuko@gmail.com>
2022-02-27 13:07:55 +0300

    refactored whois-client for make it more testable

--- observer/extension/autobot/whois/whois.go
+++ observer/extension/autobot/whois/whois.go
@@ -14,10 +14,6 @@ import (
 	httpTransport "fort.plus/transport"
 )
 
-var (
-	carrier im.Carrier
-)
-
 type query struct {
 	Query string `json:"query"`
 }
@@ -39,35 +35,45 @@ func (b *dcimBotClient) get(message repository.RegExComparator) {
 	msg := im.Cast(message)
 	re := regexp.MustCompile("/whois (.*)")
 	match := re.FindStringSubmatch(msg.Text)
-
-	// build query
 	q := query{match[0]}
+	response := b.fromService(q)
+	b.carrier.Send(msg.From, response)
+}
 
-	// send request to service
+// fromService send http-request to whois-service and parse response
+func (b *dcimBotClient) fromService(q query) string {
+	// masrhal request to json-bytes
 	raw, err := json.Marshal(q)
 	if err != nil {
-		b.carrier.Send(msg.From, fmt.Sprintf("can' marshal JSON query, %s", err))
-		return
+		return fmt.Sprintf("can' marshal JSON query, %s", err)
 	}
 
+	// send request to service
 	resp, err := httpTransport.Request(http.MethodGet, b.serverUri+"/api/v1/whois", bytes.NewBuffer(raw))
 	if err != nil {
-		b.carrier.Send(msg.From, fmt.Sprintf("failed then request dcim/whois service, %s", err))
-		return
+		return fmt.Sprintf("failed then request dcim/whois service, %s", err)
+	}
+
+	if resp.StatusCode == http.StatusNoContent {
+		return fmt.Sprintf("get status code: 204 (no content) from whois-service for query:%v", q.Query)
+	}
+	if resp.StatusCode == http.StatusBadRequest {
+		return fmt.Sprintf("get status code: 404 (bad request) from whois-service for query:%v", q.Query)
+	}
+	if resp.StatusCode == http.StatusInternalServerError {
+		return fmt.Sprintf("get status code: 500 from whois-service for query:%v", q.Query)
 	}
 
 	// parse response
 	rawResponse, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		b.carrier.Send(msg.From, fmt.Sprintf("failed then read bytes from dcim/whois service response, %s", err))
-		return
+		return fmt.Sprintf("failed then read bytes from dcim/whois service response, %s", err)
 	}
 
 	var response []string
 	if err := json.Unmarshal(rawResponse, &response); err != nil {
-		b.carrier.Send(msg.From, fmt.Sprintf("failed then parse JSON from dcim/whois service response, %s", err))
-		return
+		return fmt.Sprintf("failed then parse JSON from dcim/whois service response, %s", err)
 	}
 
-	b.carrier.Send(msg.From, strings.Join(response, "\n"))
+	return strings.Join(response, "\n")
 }
--- observer/extension/autobot/whois/whois_test.go
+++ observer/extension/autobot/whois/whois_test.go
@@ -1,9 +1,16 @@
 ﻿package whois
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestClient(t *testing.T) {
-
+	client := &dcimBotClient{
+		serverUri: "http://localhost:38000",
+	}
+	fmt.Println(client)
+	q := query{Query: "999"}
+	response := client.fromService(q)
+	fmt.Println(response)
 }
